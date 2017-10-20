cd02908ebd024dd6d7f386473e97793c015f5459
jtbeckha <jtbeckha@gmail.com>
2017-10-20 13:41:24 -0400

    Simple server is running, configure logback to get rid of STDOUT spam

---
+++ src/main/kotlin/Server.kt
@@ -0,0 +1,16 @@
+import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.netty.Netty
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!\n", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
